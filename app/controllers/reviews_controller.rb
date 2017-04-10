0c696fe3dfc80cae3bb9ebf678dd0d9672a59354
lance <xiakailun919@163.com>
2017-04-10 19:33:47 +0800

    add edit destroy function to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,29 @@ class ReviewsController < ApplicationController
       end
     end
 
+    def edit
+      @movie = Movie.find(params[:movie_id])
+      @review = Review.find(params[:id])
+    end
+
+    def update
+      @movie = Movie.find(params[:movie_id])
+      @review = Review.find(params[:id])
+      if @review.update(review_params)
+        redirect_to account_reviews_path, notice:"Update Success"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @movie = Movie.find(params[:movie_id])
+      @review = Review.find(params[:id])
+      @review.destroy
+      flash[:alert] = "review deleted"
+      redirect_to account_reviews_path
+    end
+
 
     private
 
